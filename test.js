493ddeda66547d9155bf2f12db718a31be6942e0
Michael Kagnew <mike.kagnew@gmail.com>
2021-02-06 16:47:07 -0500

    some bugs

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 const express = require('express'),
+    ws = require('ws'),
     http = require('http'),
     app = express();
-// use express static to deliver resources HTML, CSS, JS, etc)
-// from the public folder
-app.use(express.static('public'));
-http.createServer(options, app).listen(3355);
-console.log("The HTTP server is up and running");
+
+server = 
