ab0a925ec957753cffc1963a21af5e7dfa8e5788
kandloic <jkand011@uottawa.ca>
2021-02-06 11:30:27 -0500

    Initialized react project + installed material UI library

--- test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'),
-    http = require('http'),
-    app = express();
-// use express static to deliver resources HTML, CSS, JS, etc)
-// from the public folder
-app.use(express.static('public'));
-http.createServer(options, app).listen(3355);
-console.log("The HTTP server is up and running");
